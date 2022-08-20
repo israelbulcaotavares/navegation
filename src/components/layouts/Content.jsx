1ec2f774aed0d9e2daa13ff79d0357020a98e69f
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-08-20 20:58:36 -0300

    Página não encontrada

--- src/components/layouts/Content.jsx
+++ src/components/layouts/Content.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from "react-router-dom"; // para navegações
 import Home from "../../views/examples/Home";
 import About from "../../views/examples/About";
 import Param from "../../views/examples/Param";
+import NoutFound from "../../views/examples/Examples";
 
 const Content = (props) => (
   <main className="Content">
@@ -12,6 +13,8 @@ const Content = (props) => (
       <Route path="/about" element={<About />} />
       <Route path="/param/:id" element={<Param />} />
       <Route path="/" exact element={<Home />} />
+      
+      <Route path="*" exact element={<NoutFound />} />
 
     </Routes>
   </main>
---
+++ src/views/examples/Examples.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const NoutFound = props => (
+    <div className="NoutFound">
+        <h1>404</h1>
+        <h2>Ops... Página não encontrada!</h2>
+    </div>
+)
+export default NoutFound
