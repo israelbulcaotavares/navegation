b0eb03b3ee0fd5cc28b546fcb37033ee5134e4bd
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-08-20 18:27:05 -0300

    Aplicando estilos

--- src/components/layouts/Content.jsx
+++ src/components/layouts/Content.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Content = props => (
     <main className='Content'>
-        Componente Menu
+        Componente Content
     </main>
 )
 
--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import Content from "../components/layouts/Content";
+import Menu from "../components/layouts/Menu";
 import './App.css'
  const App = props => (
     <div className="App">
-       Componente App
+       <Menu/>
+       <Content></Content>
     </div>
 )
    
