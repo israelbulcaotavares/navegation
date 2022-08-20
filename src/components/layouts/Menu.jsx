7e8abe5f212a8a089b26e9a7ba8803ac02bf185e
israelbulcaotavares <33582958+israelbulcaotavares@users.noreply.github.com>
2022-08-20 20:50:02 -0300

    Navegação com parâmetro

--- src/components/layouts/Menu.jsx
+++ src/components/layouts/Menu.jsx
@@ -8,8 +8,9 @@ const Menu = props => (
         <nav>
             <ul>
                 <li><Link to="/">Início</Link></li>
-                <li><Link to="/about">Sobre</Link></li>
-               
+                <li><Link to="/param/123">Param #01</Link></li>
+                <li><Link to="/param/legal">Param #02</Link></li>
+                <li><Link to="/about">Sobre</Link></li> 
             </ul>
         </nav>
     </aside>
